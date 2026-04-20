297c3c2ca6c37442b065e5eecd3547c296c2e12a
hhw <2589622350@qq.com>
2026-04-20 15:36:48 +0800

    update ./tools, from localhost to 127.0.0.1

--- tools/launch.bat
+++ tools/launch.bat
@@ -1,5 +1,5 @@
 @echo off
 cd /d "%~dp0\.."
-start http://localhost:8000
+start http://127.0.0.1:8000
 python -m http.server 8000
 pause
--- tools/start.sh
+++ tools/start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd "$(dirname "$0")/.."
-xdg-open "http://localhost:8000" >/dev/null 2>&1
-python3 -m http.server 8000
+python3 -m http.server 8000&
+xdg-open "http://127.0.0.1:8000" >/dev/null 2>&1
